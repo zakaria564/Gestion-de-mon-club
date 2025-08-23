383c40efa7409e2535a43a8ee25a6f7af09bf02e
Zakaria Boukhari <taxizak81@gmail.com>
2025-08-23 15:28:36 +0000

    non rien est ce que vous voulez que je vous donne un code de mon console

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -15,6 +14,28 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID
 };
+
+// Validate the Firebase configuration
+const missingConfigKeys = Object.entries(firebaseConfig)
+  .filter(([key, value]) => !value)
+  .map(([key]) => key);
+
+if (missingConfigKeys.length > 0) {
+  const errorMessage = `
+    ****************************************************************************************
+    * FIREBASE CONFIGURATION ERROR                                                         *
+    * ------------------------------------------------------------------------------------ *
+    * The following Firebase environment variables are missing:                            *
+    *   - ${missingConfigKeys.join("\n    - ")}                                                     *
+    *                                                                                      *
+    * Please make sure you have a .env.local file in the root of your project with all the *
+    * required NEXT_PUBLIC_* variables.                                                    *
+    * The application will not work correctly until this is resolved.                      *
+    ****************************************************************************************
+  `;
+  console.error(errorMessage);
+}
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
