369bc3af6599bfacad3e72e89cb567159e042758
Zakaria Boukhari <taxizak81@gmail.com>
2025-09-10 21:50:38 +0000

    un autre pat en arrière

--- src/app/finances/players/[id]/page.tsx
+++ src/app/finances/players/[id]/page.tsx
@@ -7,6 +7,11 @@ export default function PlayerPaymentsPage() {
   const params = useParams();
   const id = typeof params.id === 'string' ? params.id : '';
 
+  if (!id) {
+    return <div>Chargement...</div>;
+  }
+
   return <PlayerPaymentsClient playerId={id} />;
 }
+
 
