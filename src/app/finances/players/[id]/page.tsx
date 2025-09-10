db0b4107c18ee52186c41b2eec34005a15be46bc
Zakaria Boukhari <taxizak81@gmail.com>
2025-09-10 21:46:04 +0000

    Try fixing this error: `Runtime Error: Error: Rendered fewer hooks than

--- src/app/finances/players/[id]/page.tsx
+++ src/app/finances/players/[id]/page.tsx
@@ -7,9 +7,6 @@ export default function PlayerPaymentsPage() {
   const params = useParams();
   const id = typeof params.id === 'string' ? params.id : '';
 
-  if (!id) {
-    return <div>Chargement...</div>;
-  }
-
   return <PlayerPaymentsClient playerId={id} />;
 }
+
