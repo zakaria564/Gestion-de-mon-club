59853144be0488f3ee4909602afd6059b8adadfc
Zakaria Boukhari <taxizak81@gmail.com>
2025-09-12 19:08:26 +0000

    Placez vos fichiers d'icônes dans un dossier de votre projet (par exempl

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ import type {NextConfig} from 'next';
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
 });
 
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,6 +90,7 @@ export default function RootLayout({
        <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0ea5e9" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
       </head>
       <body>
           <ThemeProvider
