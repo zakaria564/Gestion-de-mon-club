f130394fe87e565403aa2e62edfef2b327372e3b
Zakaria Boukhari <taxizak81@gmail.com>
2025-09-13 17:26:24 +0000

    retirer le (_for element <Primitive.div>_)

--- src/app/finances/cotisations/[id]/player-payment-history-client.tsx
+++ src/app/finances/cotisations/[id]/player-payment-history-client.tsx
@@ -215,25 +215,6 @@ export function PlayerPaymentHistoryClient({ memberName }: { memberName: string 
                                 Compléter le paiement
                             </DropdownMenuItem>
                         )}
-                        {(payment.status === 'partiel' || (payment.status === 'non payé' && payment.paidAmount > 0)) && (
-                          <AlertDialog>
-                            <AlertDialogTrigger asChild>
-                              <DropdownMenuItem onSelect={(e) => e.preventDefault()} className="text-red-600">Supprimer</DropdownMenuItem>
-                            </AlertDialogTrigger>
-                            <AlertDialogContent>
-                              <AlertDialogHeader>
-                                <AlertDialogTitle>Êtes-vous sûr?</AlertDialogTitle>
-                                <AlertDialogDescription>
-                                  Cette action ne peut pas être annulée. Cela supprimera définitivement ce paiement.
-                                </AlertDialogDescription>
-                              </AlertDialogHeader>
-                              <AlertDialogFooter>
-                                <AlertDialogCancel>Annuler</AlertDialogCancel>
-                                <AlertDialogAction onClick={() => handleDelete(payment.id)}>Supprimer</AlertDialogAction>
-                              </AlertDialogFooter>
-                            </AlertDialogContent>
-                          </AlertDialog>
-                        )}
                       </DropdownMenuContent>
                     </DropdownMenu>
                   </TableCell>
@@ -268,3 +249,5 @@ export function PlayerPaymentHistoryClient({ memberName }: { memberName: string 
     </div>
   );
 }
+
+    
