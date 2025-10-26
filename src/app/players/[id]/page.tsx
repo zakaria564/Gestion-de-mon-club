5f32be0a773e097a4dc1fd289ad70a71bb390f50
Zakaria Boukhari <taxizak81@gmail.com>
2025-10-26 18:13:00 +0000

    Console Error
    params are being enumerated. `params` should be unwrapped

--- src/app/players/[id]/page.tsx
+++ src/app/players/[id]/page.tsx
@@ -5,9 +5,7 @@ import React from 'react';
 export default function PlayerDetailPage(props: { params: { id: string } }) {
   const params = React.use(props.params);
   const { id } = params;
-
   if (!id) {
-    // Optionnel: afficher un état de chargement ou un message d'erreur si l'ID n'est pas disponible
     return <div>Chargement...</div>;
   }
 
